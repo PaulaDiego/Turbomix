81638f61bfe0c0cf9880bbb86a87a0e6cf3aa0db
PaulaDiego <paulacursocic@gmail.com>
2017-07-13 17:25:12 +0200

    commit con moq

--- UnitTestProject1/TurbomixServiceTest.cs
+++ UnitTestProject1/TurbomixServiceTest.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EjercicioEntregar2;
-
+using Moq;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class TurbomixServiceIntegracionTest
+    public class TurbomixServiceTest
     {
         private Alimento mAlimento1;
         private Alimento mAlimento2;
@@ -46,13 +46,26 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestPrepararPlato()
         {
+            var mockBasculaService = new Mock<IBascula>();
+            var mockCocinaService = new Mock<ICocina>();
+            mockBasculaService.Setup( bascula => bascula.Pesar(It.IsAny<Alimento>()))
+                .Returns((Alimento p) => p.Peso);
+
+            mockBasculaService.Setup(bascula => bascula.Pesar(It.IsAny<Alimento>()))
+                .Returns((Alimento p) => p.Peso);
+
+            mockCocinaService.Setup(cocina => cocina.Calentar(It.IsAny<Alimento>(), It.IsAny<Alimento>()))
+                .Callback((Alimento p1, Alimento p2) => { p1.Calentado = true; p2.Calentado = true; });
 
             Plato resultado = sut.PrepararPlato(mAlimento1, mAlimento2);
 
             mAlimentoReceta1.Peso = 1.5F;
             mAlimentoReceta2.Peso = 5F;
 
+            mockBasculaService.Verify(bascula => bascula.Pesar(It.IsAny<Alimento>()), Times.AtLeast(2));
+            mockCocinaService.Verify(cocina => cocina.Calentar(It.IsAny<Alimento>()), It.IsAny<Alimento>());
             Plato mPlato = new Plato(mAlimentoReceta1, mAlimentoReceta2);
+       
             Assert.AreEqual(mPlato, resultado);
         }
 
